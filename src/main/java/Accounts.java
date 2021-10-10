13685ec679d0095da7f49091355f4a2f264be070
Rehan Willemse <rehan.willemse@gmail.com>
2021-10-11 00:05:22 +0200

    Add Spring Framework

--- src/main/java/Accounts.java
+++ src/main/java/Accounts.java
@@ -1,8 +1,32 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Accounts {
   
   public static void main(String[] args) {
   
     System.out.println("First line of code for my project!!");
+    
+    try {
+  
+      //Class.forName()
+      
+      Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/accounts",
+              "Eclipse", "Rehan@405170");
+  
+      Statement statement = connection.createStatement();
+  
+      ResultSet resultSet = statement.executeQuery("select * from users");
+  
+      while (resultSet.next()) {
+        System.out.println(resultSet.getString("user_name"));
+        System.out.println(resultSet.getString("user_password"));
+      }
+    } catch (Exception e){
+      e.printStackTrace();
+    }
     
   }
 }
