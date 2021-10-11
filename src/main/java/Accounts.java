c6deca82c26ba24525151c940889c49ff7c42446
Rehan Willemse <rehan.willemse@gmail.com>
2021-10-11 10:02:02 +0200

    add the execution to the three different scenarios

--- src/main/java/Accounts.java
+++ src/main/java/Accounts.java
@@ -9,69 +9,100 @@ public class Accounts {
   public static void main(String[] args) {
   
     System.out.println("First line of code for my project!!");
-    Scanner action = new Scanner(System.in);
-    Scanner username = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
+    String login_name = "";
+    String login_pass = "";
+    String username = "";
     String action_choice;
+    boolean name_logged = false;
+    boolean password_logged = false;
     
     try {
       
       Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/accounts",
               "Eclipse", "Rehan@405170");
   
-      Statement statement = connection.createStatement();
+      Statement stmt = connection.createStatement();
   
-      ResultSet resultSet = statement.executeQuery("select * from users");
-  
-      while (resultSet.next()) {
-        System.out.println(resultSet.getString("user_name"));
-        System.out.println(resultSet.getString("user_password"));
+      System.out.println("Welcome to Discovery rewards Accounts, please login to your account");
+      System.out.println("Please enter your username, or '0' if you would like to exit:\n");
+      login_name = sc.nextLine();
+      
+      while (!name_logged && !password_logged ) {
+        while (!name_logged) {
+          String sql_name = "SELECT * FROM users WHERE user_name = '" + login_name + "'";
+          try {
+            ResultSet rs_name = stmt.executeQuery(sql_name);
+            //rs_name.next();
+            
+            if (rs_name.next()) {
+              System.out.println("Welcome!!");
+              System.out.println(rs_name.getString("user_name"));
+              name_logged = true;
+            } else {
+              System.out.println("Invalid username, please try again\n");
+              login_name = sc.nextLine();
+              continue;
+            }
+          }catch (Exception e){
+            e.printStackTrace();
+        }
+      }
+        
+        System.out.println("Please enter the password for user " + login_name + "\n");
+        login_pass = sc.nextLine();
+        while (!password_logged){
+          String sql_pass = "SELECT * FROM users WHERE user_name = '" + login_pass + "'";
+          ResultSet rs_pass = stmt.executeQuery(sql_pass);
+          if(rs_pass.next()){
+            System.out.println("Success");
+            password_logged = true;
+          } else {
+            System.out.println("Invalid password, please try again\n");
+            login_pass = sc.nextLine();
+            continue;
+          }
+        }
+        
       }
   
-      System.out.println("Please select which action you would like to do. 'view' to view your " +
+      System.out.println(login_name + "Please select which action you would like to do. 'view' to view " +
+                                 "your " +
                                  "current miles, 'add' to add newly gained miles to your account " +
-                                 "or 'withdraw' to withdraw miles for rewards");
-      System.out.println(action.nextLine());
-      action_choice = action.toString();
+                                 "or 'withdraw' to withdraw miles for rewards\n");
+      action_choice = sc.nextLine();
       
-      if(action_choice.toLowerCase() == "view"){
-        System.out.println("Please enter username:");
-        System.out.println(username.nextLine());
-        String user_check = String.valueOf(username);
-        
-        ResultSet view = statement.executeQuery("select * from miles");
-        while(view.next()){
-          if(user_check == view.toString()){
+      if(action_choice.toLowerCase().equals("view")){
+        System.out.println("Please enter username:\n");
+        username = sc.nextLine();
+        String sql_view_miles = "SELECT * FROM miles WHERE user_name = '" + username + "'";
+        ResultSet view = stmt.executeQuery(sql_view_miles);
+          if(view.next()){
             System.out.println(view.getString("user_name"));
             System.out.println(view.getString("Total_Miles"));
           }
         }
-      }
   
-      if(action_choice.toLowerCase() == "add"){
-        System.out.println("Please enter username:");
-        System.out.println(username.nextLine());
-        String user_check = String.valueOf(username);
-        
-        ResultSet add = statement.executeQuery("select * from miles");
-        while(add.next()){
-          if(user_check == add.toString()){
-            System.out.println(add.getString("user_name"));
-            System.out.println(add.getString("Total_Miles"));
-          }
+      if(action_choice.toLowerCase().equals("add")){
+        System.out.println("Please enter username:\n");
+        username = sc.nextLine();
+        String sql_view_miles = "SELECT * FROM miles WHERE user_name = '" + username + "'";
+        ResultSet view = stmt.executeQuery(sql_view_miles);
+        if(view.next()){
+          System.out.println(view.getString("user_name"));
+          System.out.println(view.getString("Total_Miles"));
+          System.out.println("Amount of miles to be added:\n");
         }
       }
   
-      if(action_choice.toLowerCase() == "withdraw"){
-        System.out.println("Please enter username:");
-        System.out.println(username.nextLine());
-        String user_check = String.valueOf(username);
-        
-        ResultSet withdraw = statement.executeQuery("select * from miles");
-        while(withdraw.next()){
-          if(user_check == withdraw.toString()){
-            System.out.println(withdraw.getString("user_name"));
-            System.out.println(withdraw.getString("Total_Miles"));
-          }
+      if(action_choice.toLowerCase().equals("view")){
+        System.out.println("Please enter username:\n");
+        username = sc.nextLine();
+        String sql_view_miles = "SELECT * FROM miles WHERE user_name = '" + username + "'";
+        ResultSet view = stmt.executeQuery(sql_view_miles);
+        if(view.next()){
+          System.out.println(view.getString("user_name"));
+          System.out.println(view.getString("Total_Miles"));
         }
       }
       
